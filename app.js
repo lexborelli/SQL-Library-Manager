9c55b2af595b50992c63bf2d3de962200dffafbf
lexborelli <alexasborelli@gmail.com>
2024-04-19 18:48:10 -0400

    created error handling for 404 by creating a new errror and setting status to 404, than put else statement for global error

--- app.js
+++ app.js
@@ -30,19 +30,22 @@ app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'pug');
 
 // middleware
-app.use(express.static(path.join(__dirname, 'public')));
 
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
+
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
-  next(createError(404));
+  const err = new Error("err");
+  err.status = 404;
+  next(err);
 });
 
 // error handler
@@ -51,9 +54,16 @@ app.use(function(err, req, res, next) {
   res.locals.message = err.message;
   res.locals.error = req.app.get('env') === 'development' ? err : {};
 
+  if(err.status === 404) {
+    // set a user-friendy message for 404, render the error template, passed the {err} as the second parameter in render method. 
+    err.message = "Sorry! We couldn't find the page you were looking for.";
+    res.status(404).render('page-not-found', { err });
+  } else {
   // render the error page
-  res.status(err.status || 500);
-  res.render('error');
+    err.message = err.message || `Something went wrong with the server.`;
+    res.status(err.status || 500).render('error', {err});
+    console.log(err.status, err.message);
+  }
 });
 
 module.exports = app;
